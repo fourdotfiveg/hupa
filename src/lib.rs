57ffaf3c5e9ea6cf2b6631bb01dd027a0e1a2e79
notkild <notkild@gmail.com>
2017-03-22 16:54:29 +0100

    Change backup_dir for category

--- src/lib.rs
+++ src/lib.rs
@@ -8,19 +8,33 @@ use std::path::{Path, PathBuf};
 ///
 /// # Arguments
 ///
-/// `backup_dir` is the directory of the backup
+/// `category` is the category of the hupa, i.e category can be os, dotfiles or else
+///
+/// `sub_category` is the sub category of the hupa, i.e if category is os, sub_category can be
+/// gentoo, fedora or windows
+///
 /// `restore_dir` is the director of the backed up directory
 pub struct Hupa {
-    backup_dir: PathBuf,
+    category: String,
+    sub_category: String,
     restore_dir: PathBuf,
 }
 
 impl Hupa {
     /// Default constructor
-    pub fn new<P: AsRef<Path>>(backup_dir: P, restore_dir: P) -> Hupa {
+    pub fn new<P: AsRef<Path>, S: AsRef<str>>(category: S,
+                                              sub_category: S,
+                                              restore_dir: P)
+                                              -> Hupa {
         Hupa {
-            backup_dir: backup_dir.as_ref().to_path_buf(),
+            category: category.as_ref().to_owned(),
+            sub_category: sub_category.as_ref().to_owned(),
             restore_dir: restore_dir.as_ref().to_path_buf(),
         }
+    }
+
+    /// Return the backup directory of the hupa
+    pub fn backup_dir(&self) -> PathBuf {
+        PathBuf::new()
     }
 }
