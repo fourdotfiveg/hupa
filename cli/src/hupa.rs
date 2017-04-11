c62d3604ea7f2738cb337018d474c280a094fe6d
notkild <notkild@gmail.com>
2017-04-11 19:22:00 +0200

    Add root check

--- cli/src/hupa.rs
+++ cli/src/hupa.rs
@@ -80,6 +80,41 @@ pub fn backup(hupas: &[Hupa]) {
 }
 
 /// Restore hupas with interface
+#[cfg(unix)]
+pub fn restore(hupas: &[Hupa]) {
+    // Needs root check
+    for hupa in hupas {
+        if hupa.needs_root() {
+            println!("Looks like some hupas needs root to be restored.");
+            let result = read_line_bool("Ignore them? [y/n]: ", "");
+            if result {
+                break;
+            } else {
+                let args = ::std::env::args()
+                    .map(|s| format!("{} ", s))
+                    .collect::<String>();
+                println!("Then run `sudo {}` to restore them or just re-run\
+                         this command without selecting those who needs root",
+                         args);
+                return;
+            }
+        }
+    }
+    for hupa in hupas {
+        if hupa.needs_root() {
+            println!("{} ignored because he needs root access",
+                     hupa.get_name().yellow());
+            continue;
+        }
+        exec_hupa(&hupa,
+                  |h| h.restore(),
+                  PrintOrder::BackupToOrigin,
+                  "Restoring");
+    }
+}
+
+/// Restore hupas with interface
+#[cfg(not(unix))]
 pub fn restore(hupas: &[Hupa]) {
     for hupa in hupas {
         exec_hupa(&hupa,
