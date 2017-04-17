84f61032983c0c35fcb092dfded3b761fcd311f1
notkild <notkild@gmail.com>
2017-04-17 17:07:15 +0200

    Implemented basic function for category

--- lib/src/category.rs
+++ lib/src/category.rs
@@ -17,4 +17,48 @@ impl Category {
             hupas: Vec::new(),
         }
     }
+
+    /// Return the category name
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+
+    /// Return the hupas list
+    pub fn get_hupas(&self) -> &Vec<Hupa> {
+        &self.hupas
+    }
+
+    /// Insert a new hupa at index
+    ///
+    /// Return the hupa if category is not the same
+    pub fn insert(&mut self, idx: usize, hupa: Hupa) -> Option<Hupa> {
+        if hupa.get_category_str() == self.name {
+            self.hupas.insert(idx, hupa);
+            None
+        } else {
+            Some(hupa)
+        }
+    }
+
+    /// Add a new hupa
+    ///
+    /// Return the hupa if category is not the same
+    pub fn push(&mut self, hupa: Hupa) -> Option<Hupa> {
+        if hupa.get_category_str() == self.name {
+            self.hupas.push(hupa);
+            None
+        } else {
+            Some(hupa)
+        }
+    }
+
+    /// Remove hupa at index
+    pub fn remove(&mut self, idx: usize) -> Hupa {
+        self.hupas.remove(idx)
+    }
+
+    /// Remove the last hupa of the category
+    pub fn pop(&mut self) -> Option<Hupa> {
+        self.hupas.pop()
+    }
 }
