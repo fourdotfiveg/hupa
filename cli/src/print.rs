0bf204d67a40c482fdec23cd895c057686969508
notkild <notkild@gmail.com>
2017-04-18 14:46:51 +0200

    Reworking on print command

--- cli/src/print.rs
+++ cli/src/print.rs
@@ -6,43 +6,42 @@ use libhupa::*;
 
 /// Print subcommand
 pub fn print_subcommand(hupas: Vec<Hupa>, sub_m: &ArgMatches) {
-    for hupa in hupas {
-        let size_b = if sub_m.is_present("size") {
-            format!(" ({})",
-                    hupa.get_backup_size()
-                        .unwrap_or(0)
-                        .file_size(DEFAULT_FSO)
-                        .expect("Error when showing size"))
-                    .bold()
-
+    let size_enabled = sub_m.is_present("size");
+    let mut categories = hupas.into_categories();
+    categories.sort();
+    for category in categories {
+        let mut sizes = Vec::new();
+        let mut total = 0;
+        let mut total_str = String::new();
+        if size_enabled {
+            for hupa in &category {
+                let size = hupa.get_backup_size().unwrap_or(0);
+                total += size;
+                sizes.push(size.file_size(DEFAULT_FSO).unwrap());
+            }
+            total_str = format!(", total {}", total.file_size(DEFAULT_FSO).unwrap());
         } else {
-            ColoredString::default()
-        };
-        let size_o = if sub_m.is_present("size") {
-            format!(" ({})",
-                    hupa.get_origin_size()
-                        .unwrap_or(0)
-                        .file_size(DEFAULT_FSO)
-                        .expect("Error when showing size"))
-                    .bold()
-        } else {
-            ColoredString::default()
-        };
-        let autobackup = if hupa.is_autobackup_enabled() {
-            format!("autobackup: {}", "enabled".green())
-        } else {
-            format!("autobackup: {}", "disabled".red())
-        };
-        println!("{}/{}{} {} {}{}:\n{}\ndescription: {}\n",
-                 hupa.get_category_str().bold(),
-                 hupa.get_name().yellow().bold(),
-                 size_b,
-                 "<->".bold(),
-                 hupa.get_origin().display().to_string().bold(),
-                 size_o,
-                 autobackup,
-                 hupa.get_desc().dimmed());
-        hupa.needs_root();
+            for _ in 0..category.len() {
+                sizes.push(String::new());
+            }
+        }
+        println!("{}: {} item(s){}",
+                 category.get_name().bold(),
+                 category.len(),
+                 total_str);
+        for (i, hupa) in category.iter().enumerate() {
+            println!(" -- {}:", hupa.get_name().yellow().bold());
+            println!("   -- origin: {}", hupa.get_origin().display());
+            if size_enabled {
+                println!("   -- backup size: {}", sizes[i]);
+            }
+            let autobackup = if hupa.is_autobackup_enabled() {
+                format!("{}", "enabled".green())
+            } else {
+                format!("{}", "disabled".red())
+            };
+            println!("   -- autobackup is {}", autobackup);
+            println!("   -- description: {}", hupa.get_desc());
+        }
     }
-
 }
