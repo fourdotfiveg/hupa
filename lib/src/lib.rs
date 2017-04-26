9468c886b77c97c104fcae7b0fe64dc8e7609c49
notkild <notkild@gmail.com>
2017-04-26 18:29:22 +0200

    Fix quick error macro

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,6 +1,7 @@
 //! Hupa is a tool to backup and restore some settings or file of your system
 
 #![deny(missing_docs)]
+#![recursion_limit="128"]
 
 #[cfg(unix)]
 extern crate libc;
@@ -17,12 +18,14 @@ mod error;
 mod fs_extra;
 mod hupa;
 mod metadata;
+mod vars;
 
 pub use category::*;
 pub use config::*;
 pub use error::*;
 pub use hupa::*;
 pub use metadata::*;
+pub use vars::*;
 
 use app_dirs::AppInfo;
 
