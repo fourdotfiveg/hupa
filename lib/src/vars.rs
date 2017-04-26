8c73037d0ac1af3258f9b62be6003f2311057c8b
notkild <notkild@gmail.com>
2017-04-26 19:03:11 +0200

    Add write

--- lib/src/vars.rs
+++ lib/src/vars.rs
@@ -1,21 +1,20 @@
 //! Vars handler
 
 use error::*;
+use std::io::{Read, Write};
 
 /// Var type, first arg is var name and second arg is var's state (enabled or disabled)
 pub type Var = (String, bool);
 
-/// Handle all vars 
+/// Handle all vars
 pub struct VarsHandler {
-    vars: Vec<Var>
+    vars: Vec<Var>,
 }
 
 impl VarsHandler {
     /// Default constructor
     pub fn new(vars: Vec<Var>) -> VarsHandler {
-        VarsHandler {
-            vars: vars
-        }
+        VarsHandler { vars: vars }
     }
 
     /// Read from buffer
@@ -43,6 +42,21 @@ impl VarsHandler {
         Ok(VarsHandler::new(vars))
     }
 
+    /// Read from stream
+    pub fn read_from_stream<R: Read>(stream: &mut R) -> Result<VarsHandler> {
+        let mut buf = String::new();
+        stream.read_to_string(&mut buf)?;
+        Self::read_from_buf(buf)
+    }
+
+    /// Write to stream
+    pub fn write_to_stream<W: Write>(&self, stream: &mut W) -> Result<()> {
+        for var in &self.vars {
+            write!(stream, "{}={}\n", var.0, var.1)?;
+        }
+        Ok(())
+    }
+
     /// Add var
     pub fn add_var(&mut self, var: Var) {
         self.vars.push(var);
@@ -53,7 +67,7 @@ impl VarsHandler {
         let var_name = var_name.as_ref();
         for i in 0..self.vars.len() {
             if self.vars[i].0 == var_name {
-                return Some(self.vars.remove(i))
+                return Some(self.vars.remove(i));
             }
         }
         None
